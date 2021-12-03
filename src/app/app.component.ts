f98c48bea252fcedd5e8a180de5c37d99368b393
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-12-04 01:46:24 +0300

    Navbar kaldırıldı, ana sayfa düzenlendi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
   title = 'ng-portfolio';
+
+  ngAfterViewInit(): void {
+    let myName = document.getElementById('myName');
+    let marsBack = document.getElementById('marsBack')
+    window.addEventListener('scroll', function () {
+      let value = window.scrollY;
+      if(myName) (myName.style.marginTop = value + 4 + 'px')
+      /*TODO ARKAPLAN SAĞA SOLA KAYABİLİR*/
+    })
+  }
 }
-
-let name = document.getElementById('myName');
-
-window.addEventListener('scroll', function () {
-  console.log(123);
-  let value = window.scrollY;
-  if(name) (name.style.marginTop = value + 4 + 'px')
-})
 
 
