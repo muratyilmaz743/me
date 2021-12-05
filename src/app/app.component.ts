fa37aa743817508436c039baa344df989a5a5718
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-12-05 21:14:18 +0300

    jquery added, arrow function redesigned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component} from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,16 @@ export class AppComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     let myName = document.getElementById('myName');
-    let marsBack = document.getElementById('marsBack')
+    let arrow = document.getElementById('bounce');
     window.addEventListener('scroll', function () {
       let value = window.scrollY;
       if(myName) (myName.style.marginTop = value + 4 + 'px')
       /*TODO ARKAPLAN SAĞA SOLA KAYABİLİR*/
-    })
+    });
+
+    $(window).scroll(function() {
+      $(arrow).css("opacity",$(document).height() - $(window).scrollTop() - $(window).height() - 50);
+    });
   }
 }
 
