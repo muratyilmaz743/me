5feb90f4f1b0b6f48a5983b30b089a02e91f2a9e
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-12-06 21:38:38 +0300

    Butonlar ve buton fonksiyonları eklendi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,23 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     let myName = document.getElementById('myName');
     let arrow = document.getElementById('bounce');
+    let button = document.getElementById('button1');
+
     window.addEventListener('wheel', function (e) {
       var opacityOfText = $(myName).css("opacity");
-      var sizeOfText = parseInt($(myName).css("font-size"),10) + e.deltaY/12.6;
-
-      if (sizeOfText < 90)(sizeOfText = 90)
-      console.log(parseInt($(myName).css("font-size"),10))
-      console.log(e.deltaY)
-      console.log(sizeOfText)
-      $(myName).css("opacity", opacityOfText - e.deltaY/1260).css("font-size", sizeOfText + "px")
-
+      var sizeOfText = parseInt($(myName).css("font-size"), 10) + e.deltaY / 12.6;
+      if (sizeOfText < 94) (sizeOfText = 94)
+      $(myName).css("opacity", opacityOfText - e.deltaY / 1260).css("font-size", sizeOfText + "px")
     });
 
     $(window).scroll(function () {
       $(arrow).css("opacity", $(document).height() - $(window).scrollTop() - $(window).height() - 50);
     });
+
+    // @ts-ignore
+    button.addEventListener("click", function () {
+
+    })
   }
 }
 
