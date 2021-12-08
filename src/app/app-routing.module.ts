972874a14acb1be13b8219b19c1c7a2ba6da6a41
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-12-08 11:17:07 +0300

    Links are restorated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,3 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [WorksPageComponent, HomePageComponent, CommunicationPageComponent, LinksPageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LinksPageComponent } from './links-page/links-page.component';
-import { CommunicationPageComponent } from './communication-page/communication-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LinksPageComponent,
-    CommunicationPageComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
