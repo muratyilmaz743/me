c1800dfa3b1a9a2130c464d03c83b734363cea54
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-12-13 22:36:43 +0300

    Logo and header

--- src/app/works-page/works-page.component.ts
+++ src/app/works-page/works-page.component.ts
@@ -10,19 +10,7 @@ export class WorksPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    /*TODO it scrolling but freezes when scrolled too much*/
-    window.addEventListener('wheel', function (e) {
-      let firstRow = document.getElementById('firstRow');
-      let secondRow = document.getElementById('secondRow');
-      if (e.deltaY < 0){
-        // @ts-ignore
-        firstRow.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-      }else {
-        // @ts-ignore
-        secondRow.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-      }
 
-    })
   }
 
 }
