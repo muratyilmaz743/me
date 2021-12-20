d8f39e1a59f7ad9cf0aa602c6d5a6f2360a04ef0
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-12-21 00:01:37 +0300

    Modal from bottom

--- src/app/works-page/works-page.component.ts
+++ src/app/works-page/works-page.component.ts
@@ -2,7 +2,7 @@ import {Component, ViewEncapsulation, OnInit, ElementRef, ViewChild} from '@angu
 
 // import Swiper core and required modules
 import SwiperCore, {Pagination, Navigation} from "swiper";
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 // install Swiper modules
 SwiperCore.use([Pagination, Navigation]);
 
@@ -17,24 +17,27 @@ SwiperCore.use([Pagination, Navigation]);
       class="mySwiper">
       <ng-template swiperSlide>
         <img src="../../assets/arcelikBackground.png" alt="arcelik"/>
-         <h2 (click)="openScrollableContent(longContent)" #arcelikHeader id="arcelikHeader" >Arçelik B2C Hybris Project</h2>
+        <h2 (click)="openScrollableContent(longContent)" #arcelikHeader id="arcelikHeader">Arçelik B2C Hybris
+          Project</h2>
       </ng-template>
       <ng-template swiperSlide>
         <img src="../../assets/underWorkBackground.jpg" alt="onwork"/>
-        <h2 (click)="onClickCarsi()" #carsiHeader id="carsiHeader">Çarşıbaşı Kozmetik ve Tekstil | SAP Hybris B2C Project
-        <i>OnWork</i>
+        <h2 (click)="onClickCarsi()" #carsiHeader id="carsiHeader">Çarşıbaşı Kozmetik ve Tekstil | SAP Hybris B2C
+          Project
+          <i>OnWork</i>
         </h2>
       </ng-template>
       <ng-template swiperSlide>
         <img src="../../assets/underWorkBackground.jpg" alt="onwork"/>
         <h2 (click)="onClickPenti()" #pentiHeadeer id="pentiHeader">Penti E-Commerce Cloud B2C Projesi
-            <i>OnWork</i>
+          <i>OnWork</i>
         </h2>
       </ng-template>
     </swiper>
     <ng-template #longContent let-modal>
       <div class="modal-body">
-        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+        <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget
+          quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-light" (click)="modal.close('Close click')">Close</button>
@@ -65,13 +68,21 @@ export class WorksPageComponent implements OnInit {
   }
 
   onClickArcelik() {
-    // @ts-ignore
-    document.getElementById('arcelikHeader').style.marginTop = "-75vh";
+    const arcelik = document.getElementById('arcelikHeader');
+    if (arcelik?.classList.contains('upped')) {
+      // @ts-ignore
+      arcelik.style.marginTop = "0";
+      arcelik.classList.remove('upped')
+    } else {
+      arcelik?.classList.add('upped');
+      // @ts-ignore
+      arcelik?.style.marginTop = "-75vh";
+    }
   }
 
   onClickCarsi() {
     // @ts-ignore
-    document.getElementById('carsiHeader').style.marginTop = "-75vh";
+    document.getElementById('carsiHeader')?.style.marginTop = "-75vh";
   }
 
   onClickPenti() {
@@ -80,7 +91,7 @@ export class WorksPageComponent implements OnInit {
   }
 
   openScrollableContent(longContent: any) {
-    this.modalService.open(longContent, { scrollable: true });
+    this.modalService.open(longContent, {scrollable: true});
     this.onClickArcelik();
   }
 }
