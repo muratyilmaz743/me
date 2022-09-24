ee6e652e65aa4cf667d92b9452c1b900f37b48f3
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-09-24 22:56:03 +0300

    Scroll efect düzenlendi

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -5,9 +5,9 @@ import {Work} from "../work";
 @Component({
   selector: 'app-works',
   template: `
-    <div class="frame"></div>
+    <div id="frame"></div>
       <ng-container *ngFor="let work of works">
-          <div class="work">  
+          <div class="work" id="{{work.name}}-work">  
             <a class="companyName" href="{{ work.baseURL }}" target="_blank"><span class="companyName" style="color: {{ work.styleColor }}">{{ work.name }}</span></a>
             <p class="description" style="color: {{ work.styleColor }}">{{ work.definition }}</p>
             <div class="wrapper">   
@@ -21,12 +21,39 @@ import {Work} from "../work";
 export class WorksComponent implements OnInit {
 
   works: Work[]= [];
+  currentPosition: number = 0;
 
   constructor(private worksService: WorksService) { }
 
   ngOnInit(): void {
+    var animationIsDone = true;
+    var works = document.getElementsByClassName("work");
+    var position = this.currentPosition;
     this.getHeroes();
     document.getElementById("worksLink")?.classList.add("current");
+  
+    window.addEventListener('wheel', function(e){
+      if(animationIsDone){    
+
+        var scrollDir = e.deltaY < 0  ? "up" : "down";
+
+        if(scrollDir === "up" && position !== 0){
+          animationIsDone = false;
+          preventScroll(e);
+          position--;
+          scroll(position,works);        
+        }else if(scrollDir === "down" && position !== works.length-1){   
+          animationIsDone = false;
+          preventScroll(e);     
+          position++;
+          scroll(position,works);
+        }
+
+        setTimeout(function() {
+          animationIsDone = true;
+      }, 2000);
+      }
+    });
   }
 
   getHeroes(): void{
@@ -34,3 +61,20 @@ export class WorksComponent implements OnInit {
       .subscribe(works => this.works = works)
   }
 }
+
+function scroll(index: number, listOfElements: HTMLCollection){
+  listOfElements[index].scrollIntoView();
+}
+
+function isInFrame(myElement: Element){
+  var boundingOfElement = myElement.getBoundingClientRect();  
+
+  return (boundingOfElement.top == 0);
+}
+
+function preventScroll(event: Event) {
+
+  event.preventDefault();
+  event.stopPropagation();
+}
+
